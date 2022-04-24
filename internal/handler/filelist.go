46099f05fd8621894eca40ef5ca2cc0bf8a20f22
ted-tech <ted@cess.one>
2022-04-24 17:30:48 +0800

    View the most recent [1-1000] filenames

--- internal/handler/filelist.go
+++ internal/handler/filelist.go
@@ -1,17 +1,22 @@
 package handler
 
 import (
-	"cess-httpservice/internal/chain"
-	"cess-httpservice/internal/db"
+	"bufio"
+	"cess-httpservice/configs"
 	. "cess-httpservice/internal/logger"
 	"cess-httpservice/internal/token"
 	"cess-httpservice/tools"
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"os"
+	"path/filepath"
+	"sort"
 	"strconv"
+	"strings"
 	"time"
 
+	"github.com/btcsuite/btcutil/base58"
 	"github.com/gin-gonic/gin"
 )
 
@@ -51,34 +56,117 @@ func FilelistHandler(c *gin.Context) {
 		return
 	}
 
-	resp.Code = http.StatusInternalServerError
-	data, err := chain.GetFilelistInfo(token_de.Walletaddr)
-	if err != nil {
-		resp.Msg = err.Error()
-		c.JSON(http.StatusInternalServerError, resp)
-		return
-	}
-	var fnames = make([]string, 0)
-	db, err := db.GetDB()
-	if err != nil {
-		resp.Msg = err.Error()
-		c.JSON(http.StatusInternalServerError, resp)
-		return
-	}
-	for i := 0; i < len(data); i++ {
-		s := ""
-		for j := 0; j < len(data[i]); j++ {
-			temp := fmt.Sprintf("%c", data[i][j])
-			s += temp
+	// Parameters
+	resp.Code = http.StatusBadRequest
+	resp.Msg = Status_400_default
+	var page, size, strartIndex = 0, 0, 0
+	var defaultPage, defaultSize = true, true
+	sizes := c.Query("size")
+	pages := c.Query("page")
+	if pages != "" {
+		page, err = strconv.Atoi(pages)
+		if err != nil {
+			Err.Sugar().Errorf("[%v] [%v] filename is empty", c.ClientIP(), usertoken.Mailbox)
+			c.JSON(http.StatusBadRequest, resp)
+			return
 		}
-		fid, err := strconv.ParseInt(s, 10, 64)
-		if err == nil {
-			b, err := db.Get(tools.Int64ToBytes(fid))
-			if err == nil {
-				fnames = append(fnames, string(b))
+		defaultPage = false
+	}
+	if sizes != "" {
+		size, err = strconv.Atoi(sizes)
+		if err != nil {
+			Err.Sugar().Errorf("[%v] [%v] filename is empty", c.ClientIP(), usertoken.Mailbox)
+			c.JSON(http.StatusBadRequest, resp)
+			return
+		}
+		defaultSize = false
+	}
+	resp.Code = http.StatusInternalServerError
+	resp.Msg = Status_500_unexpected
+	fs, err := tools.WalkDir(filepath.Join(configs.FileCacheDir, fmt.Sprintf("%v", usertoken.UserId), configs.FilRecordsDir))
+	if err != nil {
+		Err.Sugar().Errorf("[%v] [%v] %v", c.ClientIP(), usertoken.Mailbox, err)
+		c.JSON(http.StatusInternalServerError, resp)
+		return
+	}
+	sort.Strings(fs)
+	if len(fs) > 0 {
+		Err.Sugar().Errorf("[%v] [%v] [%v] not found records file", c.ClientIP(), usertoken.Mailbox, filepath.Join(configs.FileCacheDir, fmt.Sprintf("%v", usertoken.UserId)))
+		c.JSON(http.StatusInternalServerError, resp)
+		return
+	}
+	if defaultPage {
+		if defaultSize {
+			size = 30
+		} else {
+			if size > 1000 {
+				size = 1000
 			}
 		}
+		var fnamelist = make([]string, size)
+		file, err := os.Open(filepath.Join(configs.FileCacheDir, fmt.Sprintf("%v", usertoken.UserId), configs.FilRecordsDir, fs[len(fs)-1]))
+		if err != nil {
+			Err.Sugar().Errorf("[%v] [%v] %v", c.ClientIP(), usertoken.Mailbox, err)
+			c.JSON(http.StatusInternalServerError, resp)
+			return
+		}
+		defer file.Close()
+		buffer := bufio.NewReader(file)
+		for {
+			ctx, _, err := buffer.ReadLine()
+			if err != nil {
+				break
+			}
+			if strings.TrimSpace(string(ctx)) == "" {
+				continue
+			}
+			fnamelist = append(fnamelist, string(ctx))
+		}
+		if len(fnamelist) < size && len(fs) > 1 {
+			file, err := os.Open(filepath.Join(configs.FileCacheDir, fmt.Sprintf("%v", usertoken.UserId), configs.FilRecordsDir, fs[len(fs)-2]))
+			if err != nil {
+				Err.Sugar().Errorf("[%v] [%v] %v", c.ClientIP(), usertoken.Mailbox, err)
+				c.JSON(http.StatusInternalServerError, resp)
+				return
+			}
+			defer file.Close()
+			var fnamelist_pre = make([]string, 1000)
+			buffer := bufio.NewReader(file)
+			for {
+				ctx, _, err := buffer.ReadLine()
+				if err != nil {
+					break
+				}
+				if strings.TrimSpace(string(ctx)) == "" {
+					continue
+				}
+				fnamelist_pre = append(fnamelist_pre, string(ctx))
+			}
+			if (size - len(fnamelist)) > len(fnamelist_pre) {
+				fnamelist = append(fnamelist, fnamelist_pre...)
+			} else {
+				fnamelist = append(fnamelist, fnamelist_pre[(len(fnamelist_pre)+len(fnamelist)-size):]...)
+			}
+		}
+		var data_names = make([]string, 0)
+		if len(fnamelist) <= size {
+			for i := range fnamelist {
+				data_names = append(data_names, string(base58.Decode(fnamelist[i])))
+			}
+		} else {
+			for i := 0; i < size; i++ {
+				data_names = append(data_names, string(base58.Decode(fnamelist[len(fnamelist)-size+i])))
+			}
+		}
+		resp.Code = http.StatusOK
+		resp.Msg = "success"
+		resp.Data = data_names
+		return
+	} else {
+		strartIndex = page * 30
 	}
+
+	// strartIndex :=
 	resp.Code = http.StatusOK
 	resp.Msg = "success"
 	//resp.Data = fnames
